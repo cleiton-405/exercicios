08301435fbf5ed76865b3f32980635f5e2e82b0d
Cleiton <cleiton4055@hotmail.com>
2025-03-24 18:14:46 -0300

    commit correção

---
+++ exercicio-1.js
@@ -0,0 +1,14 @@
+while(true){
+
+    let nota = parseFloat(prompt("Digite uma nota entre 0-10"))
+
+    if(nota < 0 || nota > 10 || isNaN(nota)){
+        alert("Número inválido, digite novamente")
+        continue
+    }
+
+    console.log("Número válido: "+nota)
+    console.log("FIM DO CÓDIGO")
+    break
+
+    }
